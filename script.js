904bd7b21f966709880f5eb30da8e3fdb84d30f1
Josh Scoville <ag4sm@outlook.com>
2022-12-13 23:04:50 -0500

    finish arrays and function

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ const holidays = ['New Years Day','Valentines Day',
 const emotions = ['happy','angry','sad','exasperated','jubilant','scared'];
 const mascots = ['Santa Claus','Easter Bunny','Jesus','Uncle Sam,','Zombie','Witch']
 
+function randomMessage() {
+    let randH = holidays[Math.floor(Math.random() * holidays.length)];
+    let randEm = emotions[Math.floor(Math.random() * emotions.length)];
+    let randMas = mascots[Math.floor(Math.random() * mascots.length)];
+    
+    console.log(`Tomorrow is ${randH} and you are feeling ${randEm} about seeing ${randMas}.`);
+};
+
+randomMessage();
