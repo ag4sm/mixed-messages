1b5504856eb4c581da72c825a3bfe4543451242d
Josh Scoville <ag4sm@outlook.com>
2022-12-13 22:53:17 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Mixed Messages
+## Random message generator
+
+This is written in Javascript.  The plan is to randomly combine 3 elements of arrays to display a crazy randomized message.
---
+++ script.js
@@ -0,0 +1,9 @@
+// build arrays to hold various statement parts
+const holidays = ['New Years Day','Valentines Day',
+                    'St Patricks Day','Easter',
+                    'Passover','Memorial Day',
+                    'Independance Day','Labor Day',
+                    'Halloween','Christmas'];
+const emotions = ['happy','angry','sad','exasperated','jubilant','scared'];
+const mascots = ['Santa Claus','Easter Bunny','Jesus','Uncle Sam,','Zombie','Witch']
+
